ddd8c23f84b4aadd47d469cd20dbbf6c127134fe
EranDaniel98 <erand1998@gmail.com>
2021-12-23 18:44:17 +0200

    Added get_shoes_data func - name and prices

--- stockX_crawler.py
+++ stockX_crawler.py
@@ -9,7 +9,7 @@ from requests.api import head
 
 class StockX_crawler:
 	def __init__(self):
-		self.url = "https://stockx.com/"
+		self.base_url = "https://stockx.com/"
 
 		self.headers = self.get_params()
 		self.get_shoes_data('retro-jordans')
@@ -17,20 +17,28 @@ class StockX_crawler:
 	def get_max_pages(self, url):
 		html_page = requests.get(url,headers=self.headers)
 		soup = bs(html_page.content.decode("utf-8"), "html.parser")
-		return soup.find_all('a',{'class':'css-1sg3yt8-PaginationButton'})[-1].getText()
+		return int(soup.find_all('a',{'class':'css-1sg3yt8-PaginationButton'})[-1].getText())
 
 
 	def get_shoes_data(self, brand):
-		shoe_url = self.url + brand
+		shoes_prices = []
+		shoes_name = []
+
+		shoe_url = self.base_url + brand
 		max_pages = self.get_max_pages(shoe_url)
 
-		for page in range(1, max_pages+1):
-			page_url = self.url + f'?page={page}'
-			content = page_url.content.decode("utf-8")
-			soup = bs(content, "html.parser")
-			shoes_prices = [x.getText() for x in soup.find_all('p',{'class':'chakra-text css-1kph905'})]
+		for page in range(1, 2+1):
+			page_url = self.base_url + brand + f'?page={page}'
+			res = requests.get(page_url, headers=self.headers)
+
+			soup = bs(res.content.decode("utf-8"), "html.parser")
+			shoes_prices = shoes_prices + [x.getText() for x in soup.find_all('p',{'class':'chakra-text css-1kph905'})]
+			shoes_name = shoes_name + [x.getText() for x in soup.find_all('p',{'class':'css-1x3b5qq'})]	
+			print(res.url)
+		
+		for i, j in zip(shoes_name, shoes_prices):
+			print(i +' - ' + j)
 			
-			print(page_url.url)
 
 
 

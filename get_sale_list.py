e116e735317b8fe9eda992740c2c2e655d6d4c64
EranDaniel98 <41913344+EranDaniel98@users.noreply.github.com>
2022-01-07 21:39:33 +0200

    Working! final crawling

--- get_sale_list.py
+++ get_sale_list.py
@@ -1,12 +1,29 @@
+from os import error, spawnle
+from pandas._libs.missing import NA
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium_stealth import stealth
 
 from bs4 import BeautifulSoup as bs
-import time, re, json
+from time import sleep
+import re
 
 class Shoe_data:
+    #chakra-modal__close-btn css-1iqbypn 'chakra-modal__close-btn css-1qgkntd'
+    close_popup_script = "document.getElementsByClassName('chakra-modal__close-btn css-1iqbypn')[0].click()"
+    close_popup_script2 = "document.getElementsByClassName('chakra-modal__close-btn css-1qgkntd')[0].click()"
+    view_sales_script = "document.getElementsByClassName('chakra-button css-2yrtpe')[2].click()"
+    sales_table_script =  "return document.getElementsByClassName('css-aydg0x')[0].innerHTML"
+    scroll_down = "window.scrollTo(0, document.body.scrollHeight)"
+    get_historical_data_script = "return document.getElementsByClassName('css-79elbk')[0].innerHTML"
+
+    popup_error_msg = "Could not locate the pop up window/s"
+    view_sales_error_msg = "Could not get last sales"
+    scroll_down_err = "Could not scroll down"
+
     def __init__(self):
         chrome_options = Options()
         chrome_options.add_argument("--window-size=1920,1080")
@@ -14,40 +31,52 @@ class Shoe_data:
         self.driver = webdriver.Chrome(options=chrome_options)
         stealth(self.driver, languages=["en-US", "en"], vendor="Google Inc.", platform="Win32", webgl_vendor="Intel Inc.", renderer="Intel Iris OpenGL Engine", fix_hairline=True)
 
-        self.close_popup_script = "document.getElementsByClassName('chakra-modal__close-btn')[0].click()"
-        self.close_popup_script2 = "document.getElementsByClassName('chakra-modal__close-btn css-teac1m')[0].click()"
-        self.view_sales_script = "document.getElementsByClassName('chakra-button css-2yrtpe')[2].click()"
-        self.sales_table_script =  'return document.getElementsByClassName("css-aydg0x")[0].innerHTML'
-        self.scroll_down = "window.scrollTo(0, document.body.scrollHeight)"
-        self.get_historical_data_script = "return document.getElementsByClassName('css-79elbk')[0].innerHTML"
-        self.popup_error_msg = 'Could not locate the pop up window/s'
+        self.size_price_dict = {}
+        self.history_data = []
 
     def get_all_data(self, shoe_url):
-        print(shoe_url)
         self.driver.get(shoe_url)
-        time.sleep(2)
+        sleep(2)
 
         try:
-            self.driver.execute_script(self.close_popup_script2)
-            time.sleep(1)
             self.driver.execute_script(self.close_popup_script)
+            sleep(2)
+            self.driver.execute_script(self.close_popup_script2)
+            sleep(1)
         except:
             print(self.popup_error_msg)
-        
-        time.sleep(1)
-        
-        self.driver.execute_script(self.scroll_down)
-        time.sleep(2)
-        historical_data = self.driver.execute_script(self.get_historical_data_script)
-        time.sleep(2)
-        self.parse_historical_data(historical_data)
-        
-        time.sleep(1)
-        self.driver.execute_script(self.view_sales_script)
+            
+        try:
+            self.driver.find_element_by_xpath("//*[contains(text(),'View Sales')]").click() # View last sales
+            sleep(2)
 
-        time.sleep(2)
-        sales_table = self.driver.execute_script(self.sales_table_script)
-        self.parse_sales_table(sales_table)
+            sales_table = self.driver.find_element_by_class_name('css-aydg0x').get_attribute("innerHTML") # Get last sales HTML and parse it
+            self.parse_sales_table(sales_table)
+            self.driver.find_element_by_class_name('chakra-modal__close-btn').click() # Close last sales window
+
+        except:
+            print(self.view_sales_error_msg)
+            return
+
+        sleep(2)
+        try:
+            self.driver.execute_script(self.scroll_down)
+            sleep(1)
+            self.driver.execute_script(self.scroll_down)
+        
+        except:
+            print(self.scroll_down_err)
+            return
+
+        sleep(4)
+        try:
+            historical_data = self.driver.find_element_by_class_name('css-79elbk').get_attribute("innerHTML")
+            self.parse_historical_data(historical_data)
+        
+        except:
+            print("Unable to locate item in html")
+            return
+
 
     def parse_sales_table(self, sales_table):
         soup = bs(sales_table,"html.parser")
@@ -55,29 +84,50 @@ class Shoe_data:
         table = [x.getText() for x in soup.find_all('p',{'class':'chakra-text'})]
         del table[3::5] #remove price tag
         del table[1::4] #remove hour stamp
-        table[::3] = [x for x in zip(table[::3],table[2::3])] # Date and price
-        del table[2::3] #remove price
-        table = table[::-1]
+        del table[::3] #remove date
+        table[1::2] = [x[1:] for x in table[1::2]] # remove $ sign
 
-        size_price_dict = {i:[] for i in table[::2]}
+        self.size_price_dict = {}
+        self.size_price_dict = {i:[] for i in table[::2]}
         
         # Create dict
         index = 0
         for key in table[::2]:
-            size_price_dict[key].append(table[index+1])
+            self.size_price_dict[key].append(table[index+1])
             index += 2
 
-        print(len(size_price_dict))
+        for key in self.size_price_dict:
+            self.size_price_dict[key] = list(map(lambda s: int(s.replace(',','')), self.size_price_dict[key]))
+            self.size_price_dict[key] = sum(self.size_price_dict[key])/ len(self.size_price_dict[key])
 
     def parse_historical_data(self, history_data):
+        self.history_data = []
         soup = bs(history_data,"html.parser")
 
         data_root = soup.find('div',{'class':'css-ldihrc'})
         children_data = [x.getText() for x in data_root.findChildren('dd',{'class':'chakra-stat__number css-jcr674'})]
-        for i in children_data:
-            print(list(map(lambda x: ''.join(x.split(',')),re.findall('([\d,]+)',i))))
+        
+        self.history_data = list(map(self.fix,self.splitStr(children_data)))
+        
+        #for i in children_data:
+        #   self.history_data.append(''.join(list(map(lambda x: x.replace(',',''), re.findall('(\d[\d,]*)',i)))))
+
+        # min_12, max_12, min_all, max_all, vol, sales, pricePremium, avg_sale_price
     
-    def get_login_info(self):
-        f = open('crawl_helper.json')
-        data = json.load(f)
-        return data['login_info']['username'], data['login_info']['password']
+    def splitStr(self, li):
+      res = []
+      for i, item in enumerate(li):
+        match = re.findall('[\$]*([\d,]+)[\%]*',item)
+        if match:
+          res += match
+        else:
+            if i < 2:
+                res += ['NaN']*2
+            else:
+                res += ['NaN']
+
+      return res
+
+    def fix(self, s):
+      s =  ''.join([l for l in s.strip() if l.isdigit()])
+      return s if s else 'NaN'
--- stockX_crawler.py
+++ stockX_crawler.py
@@ -8,16 +8,16 @@ from get_sale_list import Shoe_data
 
 class StockX_crawler:
 	def __init__(self):
-		self.retail_prices, self.shoes_name, self.shoes_urls, self.last_sale, self.volality, self.num_of_sales, self.avg_sale_price  =[],[],[],[],[],[],[]
+		#self.retail_prices, self.shoes_name, self.shoes_urls, self.last_sale, self.volality, self.num_of_sales, self.avg_sale_price  =[],[],[],[],[],[],[]
+		self.release_date = []
+		self.row_data = []
+
 		self.base_url = "https://stockx.com"
-		self.df = pd.DataFrame()
+		self.df = pd.read_csv('Items_Data.csv')
 
 		self.headers = self.get_request_params()
 		self.brands_urls = self.get_brand_urls()
-		
-		#for url in self.brands_urls:
-		#	print('\n'+ url)
-		#	self.get_shoes_urls(url)
+
 	def get_shoe_link(self):
 		with open('shoes_url_list.txt',"r") as f:
 			links = f.readlines()
@@ -33,26 +33,50 @@ class StockX_crawler:
 	def get_shoes_data(self):
 		data = Shoe_data()
 
-		for i in range(5):
+		for i in range(5): # 5 shoes at a time
 			shoe_link = self.get_shoe_link()
 			res = requests.get(shoe_link, headers=self.headers)
-			print(res, shoe_link)
 			soup = bs(res.content.decode(), "html.parser")
+			print(f"{res} Getting data from: {shoe_link}")
 
 			name = [x.getText() for x in soup.find('h1',{'class':'chakra-heading css-146c51c'})]
-			self.shoes_name += ["".join(name[::len(name) - 1])] #name and color
-			print(len(self.shoes_name))
+			prod_detail = [x for x in soup.find_all('p',{'class':'chakra-text css-imcdqs'})]
+			
+			try:
+				self.shoes_name = [" ".join(name[::len(name) - 1])] #name and color
+				self.last_sale = [x.getText().split()[0] for x in soup.find('p',{'class':'chakra-text css-xfmxd4'})]
+				data.get_all_data(shoe_link)
+			except:
+				print(f"Could not get all data from:{res.url}\n")
+				with open('err.txt','a') as f:
+					f.write(shoe_link + '\n')
+				continue	
+			
+			try:
+				self.row_data = [self.shoes_name[0], prod_detail[3].getText(), prod_detail[2].getText()[1:], self.last_sale[0]] #name, releaseDate, retailPrice, lastSale
+				self.row_data += [data.history_data[4], None, None, data.history_data[5], data.history_data[7], data.history_data[0], data.history_data[1], data.history_data[2], data.history_data[3]]
+			except:
+				print("Could not get retail price and or release date\n")
+				with open('err.txt','a') as f:
+					f.write(shoe_link + '\n')
+				continue
 
-			data.get_all_data(shoe_link)
+			for k in data.size_price_dict:
+				self.row_data[5] = k
+				self.row_data[6] = data.size_price_dict[k]
+			
+				df = pd.DataFrame([self.row_data])
+				df.to_csv("Items_Data.csv", header=False,index=False, mode='a')
+
+			print(f"done with {self.shoes_name[0]}, Moving on next\n")
 		
 	def get_max_pages(self, url):
 		html_page = requests.get(url, headers=self.headers)
 		soup = bs(html_page.content.decode("utf-8"), "html.parser")
 		return int(soup.find_all('a',{'class':'css-1sg3yt8-PaginationButton'})[-1].getText())
 
-	def get_shoes_urls(self, brand_url):
+	def create_shoes_urls_file(self, brand_url):
 		max_pages = self.get_max_pages(brand_url)
-		f = open('shoes_url_list.txt','w')
 		
 		for page in range(max_pages+1):
 			page_url = brand_url + f'?page={page + 1}'
@@ -60,11 +84,12 @@ class StockX_crawler:
 			print(res, page_url)
 
 			soup = bs(res.content.decode(), "html.parser")
-			self.shoes_urls = self.shoes_urls + [x.find('a')['href'] for x in soup.find_all('div',{'class':'product-tile css-1trj6wu-TileWrapper'})]
+			self.shoes_urls += [x.find('a')['href'] for x in soup.find_all('div',{'class':'product-tile css-1trj6wu-TileWrapper'})]
 
-			f.writelines([self.base_url + link + '\n' for link in self.shoes_urls])
+		self.shoes_urls = list(dict.fromkeys(self.shoes_urls))
 		
-		f.close()
+		with open('shoes_url_list.txt','w') as f:
+			f.writelines([self.base_url + link + '\n' for link in self.shoes_urls])
 
 	def get_brand_urls(self):
 		f = open('crawl_helper.json')
@@ -78,8 +103,23 @@ class StockX_crawler:
 
 def main():
 	crawl = StockX_crawler()
-	crawl.get_shoes_data()
-	print("FINISHED")
+
+	with open('shoes_url_list.txt') as f:
+		file_lines_count = len(f.readlines())
+
+	if(file_lines_count == 0):
+		for url in crawl.brands_urls:
+			print('\n'+ url)
+			crawl.create_shoes_urls_file(url)
+	
+	with open('shoes_url_list.txt') as f:
+		file_lines_count = len(f.readlines())
+
+	for i in range(file_lines_count//5):	
+		crawl.get_shoes_data()
+		print("finished 5 shoes")
+
+	print("\nFINISHED")
 	
 
 if __name__ == "__main__":

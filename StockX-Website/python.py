1440c80c2893ef632fedba560842ac6aa7400490
EranDaniel98 <41913344+EranDaniel98@users.noreply.github.com>
2022-01-26 16:16:33 +0200

    Finished

--- StockX-Website/python.py
+++ StockX-Website/python.py
@@ -2,16 +2,17 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('./')
+@app.route('/')
 def index():
     return render_template('prediction.html')
 
-@app.route('/get-text', methods=['GET', 'POST'])
+@app.route('/get-text', methods=['POST'])
 def foo():
     url_ = request.form['input_url']
-    return 'Hello %s have fun learning python <br/> <a href="/">Back Home</a>' % url_
+    return url_
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 3000)
     
     
+print(foo())
